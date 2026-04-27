bd32af6e8c042c48e8d56782dd4634e3f0fd8d1a
shible77 <shible0805@gmail.com>
2026-04-28 00:14:09 +0600

    Refactor CORS configuration to simplify origin handling and ensure consistency

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,24 +11,14 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const server = http.createServer(app);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://a-real-time-todo-app.onrender.com",
-];
 app.use(errorHandler);
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://a-real-time-todo-app.onrender.com",
   credentials: true
 }));
 
 const io = new Server(server, {
-  cors: { origin: "https://a-realtime-to-do-app.onrender.com" } 
+  cors: { origin: "https://a-real-time-todo-app.onrender.com" } 
 });
 
 socketHandler(io);
